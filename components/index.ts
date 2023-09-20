beacbd763e9471dcdd364a96ffabab5018b8159b
unknown <tonysonic1227@gmail.com>
2023-09-20 11:56:38 -0300

    tailwindCss

---
+++ components/index.ts
@@ -0,0 +1,26 @@
+"use client"
+import CustomButton from './CustomButton'
+import Footer from './Footer'
+import Hero from './Hero'
+import SearchBar from './SearchBar'
+import CustomFilter from './CustomFilter'
+import Navbar from './Navbar'
+import SearchManufacturer from './SearchManufacturer'
+import CarCard from './CarCard'
+import CarDetails from './CarDetails'
+import SearchButton from './SearchButton'
+import ShowMore from './ShowMore'
+
+export {
+    Hero,
+    CustomButton,
+    Footer,
+    Navbar,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+    SearchButton,
+    ShowMore,
+}
